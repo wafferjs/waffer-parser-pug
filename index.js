1f6af761dae0d62810d12e74c5a019692c2b1c5d
wvffle <casper@wvffle.net>
2018-02-26 13:57:38 +0100

    removed parse function from exports scope

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const pug = require('pug');
 
 const cwd = process.cwd();
 
-module.exports = parser => {
-  const parse = (file, next, exp, options) => {
-    let html;
-    let err;
+const parse = (file, next, exp, options) => {
+  let html;
+  let err;
 
-    try {
-      html = pug.renderFile(file, options || {});
-    } catch (e) {
-      err = e;
-    }
-
-    if (exp && !err) {
-      return next(null, require('waffer-parser-html')._export(html, file));
-    }
-
-    return next(err, html);
+  try {
+    html = pug.renderFile(file, options || {});
+  } catch (e) {
+    err = e;
   }
 
+  if (exp && !err) {
+    return next(null, require('waffer-parser-html')._export(html, file));
+  }
+
+  return next(err, html);
+}
+
+module.exports = parser => {
   return { parse, ext: '.html' }
 }
